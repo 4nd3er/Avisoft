b7cabb38286f67afce29e5e6bf99b57f560f0d78
Anderson <andersonordonez455@gmail.com>
2023-05-30 16:35:19 -0500

    funcion agregada areagalpon en el modulo de galpon

---
+++ control/static/Js/areagalpon.js
@@ -0,0 +1,7 @@
+function calcuateArea() {
+ancho = document.getElementById('ancho').value;
+largo = document.getElementById('largo').value;
+area = document.getElementById('area');
+    resultado = ancho * largo;
+    area.value = resultado
+};
