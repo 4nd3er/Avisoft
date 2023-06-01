394db2adc75c5c4aeb690b49bfa58952acf18037
unknown <SENA@POPAPRCACFSD010.FormacionSena.RED>
2023-06-01 16:46:30 -0500

    Caluclo de las Capacidades Galpon

--- control/static/Js/areagalpon.js
+++ control/static/Js/areagalpon.js
@@ -2,6 +2,19 @@ function calcuateArea() {
 ancho = document.getElementById('ancho').value;
 largo = document.getElementById('largo').value;
 area = document.getElementById('area');
+capac_gall = document.getElementById('capac_gall');
+capac_bebed = document.getElementById('capac_bebed');
+capac_nidales = document.getElementById('capac_nidales');
+capac_comed = document.getElementById('capac_comed');
     resultado = ancho * largo;
-    area.value = resultado
+    area.value = resultado;
+    resultado2 = resultado * 6;
+    capac_gall.value = resultado2;
+    resultado3 = resultado2 / 25;
+    capac_bebed.value = Math.round(resultado3);
+    resultado4 = resultado2 / 5;
+    capac_nidales.value = Math.round(resultado4);
+    capac_comed.value = Math.round(resultado3);
+
+    
 };
--- control/static/Js/capgallgal.js
+++
@@ -1,8 +0,0 @@
-function calculateAves(){
-    area = document.getElementById('area').value;
-    capac_gall = document.getElementById('capac_gall');
-    taves = area * 8;
-    capac_gall.value = taves
-
-
-};
