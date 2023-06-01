628f686f7ab1da1d0d4ab6098ce8542132e79bc5
unknown <SENA@POPAPRCACFSD010.FormacionSena.RED>
2023-06-01 15:11:37 -0500

    cambio

--- control/static/Js/areagalpon.js
+++ control/static/Js/areagalpon.js
@@ -5,12 +5,3 @@ area = document.getElementById('area');
     resultado = ancho * largo;
     area.value = resultado
 };
-
-function calculateAves(){
-    area = document.getElementById('area').value;
-    capac_gall = document.getElementById('capac_gall');
-    taves = area * 8;
-    capac_gall.value = taves
-
-
-};
---
+++ control/static/Js/capgallgal.js
@@ -0,0 +1,8 @@
+function calculateAves(){
+    area = document.getElementById('area').value;
+    capac_gall = document.getElementById('capac_gall');
+    taves = area * 8;
+    capac_gall.value = taves
+
+
+};
