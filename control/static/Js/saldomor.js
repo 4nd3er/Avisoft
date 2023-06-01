362f5391363de1a293874adf9d6cbe4f85271653
unknown <SENA@POPAPRCACFSD010.FormacionSena.RED>
2023-06-01 17:32:45 -0500

    casos

--- control/static/Js/saldomor.js
+++
@@ -1,8 +0,0 @@
-function calculateSaldo(){
-cant_muertas = document.getElementById('cant_muertas').value;
-cant_descar = document.getElementById('cant:descarte').value;
-saldo = document.getElementById('saldo');
-suma = cant_muertas  * cant_descar;
-saldo.value = suma
-
-};
