a5dfe234a8be6faafae36b70dacbb8a0813e1ec7
unknown <SENA@POPAPRCACFSD010.FormacionSena.RED>
2023-06-01 15:03:08 -0500

    calculo de capacidades

--- control/static/Js/areagalpon.js
+++ control/static/Js/areagalpon.js
@@ -5,3 +5,12 @@ area = document.getElementById('area');
     resultado = ancho * largo;
     area.value = resultado
 };
+
+function calculateAves(){
+    area = document.getElementById('area').value;
+    capac_gall = document.getElementById('capac_gall');
+    taves = area * 8;
+    capac_gall.value = taves
+
+
+};
