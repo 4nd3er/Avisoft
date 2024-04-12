8ec59713b32b86d965652f94e886e9785f0ef70e
Anderson <andersonordonez455@gmail.com>
2024-04-12 15:11:03 -0500

    cambios generales

--- AviSoft/settings.py
+++ AviSoft/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-t67kh4sn(f2w@m)s=^8jg_z1ny=bvlcd0@7&*%a3e^+w249)-e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -133,6 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_FILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
