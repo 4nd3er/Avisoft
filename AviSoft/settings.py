898c03a4fe1542bb308d9122aa801872cdac5b30
Anderson <andersonordonez455@gmail.com>
2024-04-12 14:27:59 -0500

    cambio setting

--- AviSoft/settings.py
+++ AviSoft/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t67kh4sn(f2w@m)s=^8jg_z1ny=bvlcd0@7&*%a3e^+w249)-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
